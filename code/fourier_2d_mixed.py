0c978a539992cdff6d62fd62e48d66b5f8802ab6
luiscoding <lu.zhang.code@gmail.com>
2021-10-18 17:25:28 -0400

    loss inner

--- code/fourier_2d_mixed.py
+++ code/fourier_2d_mixed.py
@@ -250,8 +250,8 @@ myloss = LpLoss(size_average=False)
 
 # inner loop update the last layer for each task
 # outer loop update the representation of all tasks
-optimizer_meta = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
-scheduler_2 = torch.optim.lr_scheduler.StepLR(optimizer_meta, step_size=step_size, gamma=gamma)
+optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer_meta, step_size=step_size, gamma=gamma)
 for ep in range(epochs):
     model.train()
     t1 = default_timer()
